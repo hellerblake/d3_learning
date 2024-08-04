66d10f1d66111cdc060be747ee4326dec6a0a3bc
Blake Heller <46772460+hellerblake@users.noreply.github.com>
2024-08-04 16:34:54 -0700

    use basic d3 javascript to build objects

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
-import './style.css'
+import "./style.css";
 
-const canvas = d3.select('.cavas');
+const canvas = d3.select(".cavas");
 
-canvas.append('svg')
+const svg = canvas.append("svg").attr("height", 600).attr("width", 600);
+
+svg
+  .append("rect")
+  .attr("width", 200)
+  .attr("height", 100)
+  .attr("fill", "blue")
+  .attr("x", 20)
+  .attr("y", 20);
+
+svg
+  .append("circle")
+  .attr("r", 50)
+  .attr("cx", 300)
+  .attr("cy", 70)
+  .attr("fill", "pink");
+
+svg
+  .append("line")
+  .attr("x1", 370)
+  .attr("x2", 400)
+  .attr("y1", 20)
+  .attr("y2", 120)
+  .attr("stroke", "red");
