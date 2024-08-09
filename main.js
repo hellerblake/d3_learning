8f20bf563c216236adf653bacb783f2fcb37e3dc
Blake Heller <46772460+hellerblake@users.noreply.github.com>
2024-08-08 18:10:10 -0700

    modify mutilble elements

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 import "./style.css";
-const data = [{ width: 200, height: 100, fill: "purple" }];
+const data = [
+  { width: 200, height: 100, fill: "purple" },
+  { width: 100, height: 60, fill: "pink" },
+  { width: 50, height: 30, fill: "red" },
+];
 
 const svg = d3.select("svg");
-
+svg.append("rect");
+svg.append("rect");
 const rect = svg
-  .select("rect")
+  .selectAll("rect")
   .data(data)
   .attr("width", (d) => d.width)
   .attr("height", (d) => d.height)
